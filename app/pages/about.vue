0d428500f4e4288f9f57cee0ae9c943519744b2c
Gabe McGuire <gabepmcguire@gmail.com>
2024-10-07 11:05:52 -0400

    add jwt

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,4 +1,30 @@
 <template>
-  <h1>about</h1>
+  <div class="flex justify-center items-center min-h-screen ">
+    <div class="p-8 rounded-lg shadow-md max-w-2xl">
+      <h1 class="font-bold text-4xl mb-6 text-center">About</h1>
+      <div class="space-y-4">
+        <p class="text-lg ">
+        I created this site to help students like myself study.
+        </p>
+        <p class="text-lg ">
+          <strong class="">Important Note:</strong> This site does not distribute any course materials. All questions are original.
+        </p>
+        <div class="mt-8 p-4 rounded-lg">
+          <p class="text-lg mb-4">
+            If you found this site helpful, perhaps consider buying me a coffee  
+          </p>
+          <a 
+            href="https://buymeacoffee.com/realgabe" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            class="inline-flex items-center px-6 py-3 bg-yellow-400 text-yellow-900 font-bold rounded-full hover:bg-yellow-300 transition duration-300"
+          >
+            <span class="mr-2">☕</span>
+            Buy Me a Coffee
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
